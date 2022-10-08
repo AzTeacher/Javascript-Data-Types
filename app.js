ae1d107aa791dc3f45a4caffa3c1eb6c155fbf31
azteacher <az.teachers.2020@gmail.com>
2022-10-08 23:15:08 +0300

    Javascript Data Types

---
+++ app.js
@@ -0,0 +1,113 @@
+                      // Javascipt Data Types 
+// ***************************************************************************
+
+                    // 1. Primitive Types
+
+// String
+// let firstName = "aziz"
+
+// console.log(firstName)
+// console.log(typeof firstName)
+
+// Numbers
+
+// let age = 30
+// console.log(age)
+// console.log(typeof age)
+
+// let x= "10";
+// let y="20";
+// console.log(x+y);
+
+// Booleans
+// let x = false;
+
+// if (x){
+//     console.log("true")
+//     console.log(typeof x)
+// }else {
+//     console.log("false")
+//     console.log(typeof y)
+// }
+
+// // Undefined
+// let age;
+// console.log(age);
+// console.log(typeof age)
+
+// if (age === undefined){
+//     console.log("You cannot added value in variable")
+// }else {
+//     console.log("error")
+// }
+
+// Null
+// let city = null;
+// console.log(city)
+// console.log(typeof city)
+
+
+
+// ***************************************************************************
+
+                    // 2. Reference Types
+
+// Arrays
+// let cars = ["Mercedes", "Audi", "Bmw", "Porsche"]
+
+// console.log(cars)
+// console.log(cars.length)
+// console.log(cars[2])
+// console.log(cars[3])
+
+// // Objects
+// let users = {
+//     firstName: "Aziz", lastName: "Amiraslanov", age: 30
+// }
+
+// console.log(users)
+// users.age = 35
+// console.log(users)
+
+
+// let customers = [
+//     { firstName: "Aziz", lastName: "Amiraslanov", age: 30},
+//     { firstName: "Mehemmed", lastName: "Aliyev", age: 35},
+//     { firstName: "Elsen", lastName: "Memmedov", age: 25}
+// ]
+
+// console.log(customers)
+
+// customers.push({ firstName: "Tural", lastName: "Memmedov", age: 15})
+// console.log(customers)
+
+
+// // Function
+
+// function divison () {
+// let n1 = 50;
+// let n2 = 10;
+// let result = n1 / n2;
+// console.log(result)
+// }
+
+// divison();
+// // Dates
+// let dates = new Date();
+// console.log(dates)
+
+// console.log(dates.getDate())
+// console.log(dates.getFullYear())
+
+// console.log(dates.setDate(9))
+// console.log(dates.setFullYear(2023))
+
+// dates.setDate(9)
+// dates.getFullYear(2023)
+
+// console.log(dates.getDate())
+// console.log(dates.getFullYear())
+
+
+// ***************************************************************************
+
